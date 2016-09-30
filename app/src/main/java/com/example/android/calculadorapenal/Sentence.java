e98229bd527ff742ab95b3da90998a8625f237b4
Victor Serpa <Victor Serpa>
2016-09-29 23:08:06 -0300

    Added Sentence class, changed MainActivity design

---
+++ app/src/main/java/com/example/android/calculadorapenal/Sentence.java
@@ -0,0 +1,30 @@
+package com.example.android.calculadorapenal;
+
+/**
+ * Created by victo on 29/09/2016.
+ */
+
+public class Sentence {
+
+    /*Variables that will hold the sentence after the operations*/
+    private int year;
+    private int month;
+    private int day;
+
+   /*Variables that will hold the original sentence*/
+    private int oYear;
+    private int oMonth;
+    private int oDay;
+
+
+    public Sentece(int year, int month, int day){
+        this.year=year;
+        this.month=month;
+        this.day=day;
+        oYear=year;
+        oMonth=month;
+        oDay=day;
+    }
+
+    private void originalDate(int )
+}
